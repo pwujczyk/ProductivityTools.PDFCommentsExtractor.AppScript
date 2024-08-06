c788c3975ad74ab384f2193ec4b06ff03da29c84
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-08-06 20:35:41 +0200

    clasp

--- src/Kod.js
+++ src/Kod.js
@@ -1,3 +1,15 @@
 function myFunction() {
-  
+  var bookhighligthsDirectory = DriveApp.getFoldersByName("Newsweek").next();
+  console.log("started")
+  var sourceFileName="pawel";
+  var bookhighligthsFiles = bookhighligthsDirectory.getFiles()
+  while (bookhighligthsFiles.hasNext()) {
+    file = bookhighligthsFiles.next();
+    sourceFileName = file.getName();
+    console.log(sourceFileName);
+    //     console.log(file.getId())
+  }
+  console.log("Returning")
+  return sourceFileName
+
 }
